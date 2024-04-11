1ced13461c8cd76f023c97f394921071938ead4a
Kenneth Garcia <kennethgarcia12@hotmail.com>
2024-04-11 15:59:57 -0700

    if applied, this commit adds smooth scrolling to the home section buttons

--- dist/resources/js/main.js
+++ dist/resources/js/main.js
@@ -25,6 +25,19 @@ $('#main-nav a').on('click', function(event){
     }
 });
 
+//Home buttons smooth Scrolling
+$('#home .btn').on('click', function(event){
+    if(this.hash !== ""){
+        event.preventDefault();
+        const hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function(){
+            window.location.hash = hash;
+        });
+    }
+});
+
 //AOS Init
 AOS.init();
 
